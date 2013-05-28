7c90c293e2dee3516a2c93b4691abddfb7309070
Lee Sanghyuck <shlee322@gmail.com>
2013-05-28 20:31:08 +0900

    Add DB SetTable Function

--- db/connecter/mysql/index.js
+++ db/connecter/mysql/index.js
@@ -72,4 +72,61 @@ module.exports = function(obj) {
             });
         });
     }
+
+    this.setTable = function(ctx, table, cb) {
+        var query = "";
+        query += "CREATE  TABLE `";
+        query += table.name;
+        query += "` (`uid` BIGINT NOT NULL , ";
+        for(var column in table.columns) {
+            var data = table.columns[column];
+
+            query += "`" + column + "` ";
+            if(data.type == "int64") {
+                query += "BIGINT NOT NULL, ";
+            } else if(data.type == "string") {
+                query += "VARCHAR(" + data.length + ") NULL, ";
+            }
+        }
+        for(var index in table.indexes) {
+            var data = table.indexes[index];
+            if(data.type != "INDEX") {
+                query += data.type + " ";
+            }
+            query += "INDEX `" + data.name + "`(";
+
+            for(var i in data.columns) {
+                var column = data.columns[i];
+                query += "`" + column[0] + "` " + column[1] + ", ";
+            }
+            query = query.substring(0, query.length - 2) + "), ";
+        }
+        query += "PRIMARY KEY (`uid`))";
+
+        pool.getConnection(function(err, conn) {
+            ctx.run(function() {
+                if(err) {
+                    throw err;
+                }
+                conn.query(query, function(err, rows) {
+                    ctx.run(function() {
+                        var e = null;
+                        try {
+                            if(err && err.code != "ER_TABLE_EXISTS_ERROR") {
+                            }
+                            if(typeof(cb)=="function") {
+                                cb(err, rows);
+                            }
+                        } catch(error) {
+                            e = error;
+                        }
+                        conn.end();
+                        if(e) {
+                            throw e;
+                        }
+                    });
+                });
+            });
+        });
+    }
 }
--- db/index.js
+++ db/index.js
@@ -8,9 +8,26 @@ module.exports = function(saram) {
 function newDB(saram) {
     this.saram = saram;
     this.addNode = addNode;
+    this.setTable = setTable;
     this.query = temp_query;
 
     this.cluster = {};
+
+    this.utill = {
+        paramToColumns : function (columns, param, type) {
+            for(var i in param) {
+                columns[param[i][0] + "_" + param[i][1]] = type;
+            }
+        },
+        paramToIndexColumns : function (indexColumns, param, type) {
+            if(!type) {
+                type = "ASC";
+            }
+            for(var i in param) {
+                indexColumns.push([param[i][0] + "_" + param[i][1], type]);
+            }
+        }
+    };
 }
 
 function addNode(nodeUrlStr, clusterName) {
@@ -84,4 +101,67 @@ function addQuery(queryName, func) {
 }
 
 function execute(queryName, obj) {
+}
+
+function setTable(ctx, data) {
+    if(typeof(data.name) != "string") {
+        throw new Error();
+    }
+    if(typeof(data.name) != "string") {
+        throw new Error();
+    }
+    var clusterName = 'default';
+    if(typeof(data.shardKey) == "string") {
+        data.shardKey = function() {return 0;}
+    }
+    if(typeof(data.shardKey) != "function") {
+        throw new Error();
+    }
+    if(typeof(data.columns) != "object") {
+        throw new Error();
+    }
+    if(!(data.indexes instanceof Array)) {
+        data.indexes = [];
+    }
+    for(var i in data.columns) {
+        var column = data.columns[i];
+        if(typeof(column) == "string") {
+            data.columns[i] = { type : column };
+            column = data.columns[i];
+        }
+        if(typeof(column) != "object") {
+            throw new Error();
+        }
+
+        if(column.type != "string" &&
+            column.type != "int64" &&
+            column.type != "sha256") {
+            throw new Error();
+        }
+
+        if(column.type == "string" && !column.length) {
+            column.length = 64;
+        }
+    }
+
+    for(var i in data.indexes) {
+        var index = data.indexes[i];
+        if(!index.type) {
+            index.type = "INDEX";
+        }
+
+        for(var columns_index in index.columns) {
+            var column = index.columns[columns_index];
+            if(typeof(column)=="string") {
+                index.columns[columns_index] = [column ,"ASC"];
+            }
+        }
+    }
+
+    //전체 노드 호출
+    //ctx.runWait();
+    var cluster = this.cluster[clusterName];
+    for(var node in cluster) {
+        cluster[node].setTable(ctx, data);
+    }
 }
