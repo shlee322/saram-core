1059751bc5e9aa80cf986cb14bc4a12ff4635fb0
Lee Sanghyuck <shlee322@gmail.com>
2013-07-11 20:38:43 +0900

    Static Module addDir 함수 추가

--- modules/static/actions.js
+++ modules/static/actions.js
@@ -1,7 +1,16 @@
 module.exports = {
     get : function (ctx) {
-        var file = ctx.current.module._content[ctx.req.path];
+        var file = getPath(ctx.current.module, ctx.req.path);
         ctx.errorTry(!file, Error);
         ctx.res.send(file);
     }
+}
+
+function getPath(module, path) {
+    var dirs = module._dirs;
+    for(var name in dirs) {
+        if(path.substring(0, name.length) === name)
+            return dirs[name] + "/" + path.substring(name.length);
+    }
+    return module._content[path];
 }
--- modules/static/init.js
+++ modules/static/init.js
@@ -4,8 +4,12 @@ function initStaticModule(ctx) {
     var _this = this;
     this._bundle = new Bundle(this);
     this._content = {};
+    this._dirs = {};
     this.addFile = function (path, file) {
         _this._content[path] = file;
+    }
+    this.addDir = function (path, dir) {
+        _this._dirs[path] = dir;
     }
 }
 
