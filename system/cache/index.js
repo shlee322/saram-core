5ffa4b84b9c24c8f28d31931e26894044e50881c
Lee Sanghyuck <shlee322@gmail.com>
2013-07-11 20:38:11 +0900

    Cache System Key 분산 개발

--- system/cache/index.js
+++ system/cache/index.js
@@ -1,13 +1,19 @@
+var XXHash = require('xxhash');
+
 exports.set = function (ctx, key, value, callback) {
+    var hash = XXHash.hash(new Buffer(key), 0x654C6162);
+
     var clusterName = "default";
 
     var cluster = ctx.getSaram().cache.getCluster(clusterName);
-    return cluster[0].set(key, value, callback);
+    return cluster[hash%cluster.length].set(key, value, callback);
 }
 
 exports.get = function (ctx, key, callback) {
+    var hash = XXHash.hash(new Buffer(key), 0x654C6162);
+
     var clusterName = "default";
 
     var cluster = ctx.getSaram().cache.getCluster(clusterName);
-    return cluster[0].get(key, callback);
+    return cluster[hash%cluster.length].get(key, callback);
 }
