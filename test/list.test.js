ba6cd443c77ef780ba1257cb93e6030048d9fd12
Lee Sanghyuck <shlee322@gmail.com>
2013-09-14 16:46:02 +0900

    List 모듈 테스트 코드 추가

--- test/list.test.js
+++ test/list.test.js
@@ -3,7 +3,7 @@ describe('Basic Module Test', function() {
         var saram = require('saram-core');
         var listModule = require('saram-core/modules/list/index.js');
 
-        it('start', function() {
+        it('#start', function() {
             var server = saram();
             server.cache.addNode("memory:///");
             server.db.addNode("mysql://travis@127.0.0.1/saram_test");
@@ -14,6 +14,18 @@ describe('Basic Module Test', function() {
             server.protocol.addProtocol("http", { port : 7000 });
 
             server.start();
+
+            it('#Insert', function() {
+                request.post('http://127.0.0.1:7000/list/', {value:'test'},  function (error, response, body) {
+                    console.log(body);
+
+                    it('#List', function() {
+                        request.get('http://127.0.0.1:7000/list/',  function (error, response, body) {
+                            console.log(body);
+                        });
+                    });
+                });
+            });
         });
     });
 });
