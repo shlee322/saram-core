834a9bfdabc9bac44f0406b40ca0d8e22b6d7397
Lee Sanghyuck <shlee322@gmail.com>
2013-09-14 10:56:52 +0900

    테스트 코드 수정

--- test/example.js
+++ test/example.js
@@ -1,7 +1,7 @@
 describe('Example', function() {
     describe('calculation', function() {
         it('1+1 should be 2', function() {
-            should.equal(1+1, 2);
+            (1+1).should.equal(2);
         });
     });
 });
