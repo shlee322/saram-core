6b64a15ab9eadfb328b6a00ea3652a66a9a6ba1e
Lee Sanghyuck <shlee322@gmail.com>
2013-09-14 21:34:57 +0900

    KeyValue 모듈 테스트 코드 추가

---
+++ test/keyvalue.test.js
@@ -0,0 +1,39 @@
+var request = require('request');
+
+describe('Basic Module Test', function() {
+    describe('KeyValue', function() {
+        var saram = require('saram-core');
+        var keyvauleModule = require('saram-core/modules/keyvalue/index.js');
+
+        it('#Start', function() {
+            var server = saram();
+            server.cache.addNode("memory:///");
+            server.db.addNode("mysql://travis@127.0.0.1/saram_test");
+            server.modules.load(keyvauleModule);
+
+            server.modules.use('elab.keyvalue', 'keyvalue_test', { name:"keyvalue_test" });
+            server.modules.weld('keyvalue_test', 'keyvalue');
+            server.protocol.addProtocol("http", { port : 7002 });
+
+            server.start();
+        });
+
+        it('#Set(Insert)', function(done) {
+            request.post('http://127.0.0.1:7002/keyvalue/test', {body:"value=test"},  function (error, response, body) {
+                done();
+            });
+        });
+
+        it('#Get', function(done) {
+            request.get('http://127.0.0.1:7002/keyvalue/test',  function (error, response, body) {
+                done();
+            });
+        });
+
+        it('#Set(Update)', function(done) {
+            request.post('http://127.0.0.1:7002/keyvalue/test', {body:"value=1234"},  function (error, response, body) {
+                done();
+            });
+        });
+    });
+});
